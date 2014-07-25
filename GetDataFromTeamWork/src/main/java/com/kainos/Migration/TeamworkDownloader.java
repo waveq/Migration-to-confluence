0756a4fbba4e45d21dfa53cdedcaef0d63e5e471
waveq <nynonn@gmail.com>
2014-07-25 13:44:51 +0200

    Comment and syntas of TeamworkDownloader

--- GetDataFromTeamWork/src/main/java/com/kainos/Migration/TeamworkDownloader.java
+++ GetDataFromTeamWork/src/main/java/com/kainos/Migration/TeamworkDownloader.java
@@ -14,38 +14,62 @@ import net.sf.json.JSONSerializer;
 
 import org.apache.commons.codec.binary.Base64;
 
+/**
+ * 
+ * @author szymonre
+ *
+ */
 public class TeamworkDownloader {
-	private String apiToken;
-	private String url;
+	
+	// Beginning of teamworkurl (https://yourpage.teamwork.com/).
+	private String urlBeginning;
 	private ConfluenceManager cm;
 	private DownloadFileFromTW dfftw;
+	// Credentials used to connect to teamwork's rest API (yourapiToken:X)
+	private String credentials;
 
 	public TeamworkDownloader(String apiToken, String url) {
 		super();
-		this.apiToken = apiToken;
-		this.url = url;
+		//this.apiToken = apiToken;
+		this.credentials = apiToken + ":X";
+		this.urlBeginning = url;
+		
 
 		dfftw = new DownloadFileFromTW();
 		cm = new ConfluenceManager();
 
-		printTree(getAllProjects(url, apiToken));
+		goThroughTree(getAllProjects());
 	}
 
-	public void printTree(JSONObject mainJson) {
+	/**
+	 * Iterates through projects array.
+	 * @param mainJson - json object that contains list of all projects in TeamworkPM.
+	 */
+	public void goThroughTree(JSONObject mainJson) {
 		JSONArray array = (JSONArray) mainJson.get("projects");
 
 		Iterator i = array.iterator();
 		while (i.hasNext()) {
 			JSONObject singleProject = (JSONObject) i.next();
 			cm.CreateSpace(singleProject.get("name").toString());
-			JSONObject categoriesMainObject = getAllCategoriesFromProject(url, apiToken,
-					singleProject.getString("id"));
+			JSONObject categoriesMainObject = getAllCategoriesFromProject(singleProject.getString("id"));
 			JSONArray categoriesArray = (JSONArray) categoriesMainObject.get("categories");
 
 			getCategories(singleProject, "", "", categoriesArray);
 		}
 	}
 
+	/**
+	 * Recursive method. If passed parentId is equals to current category's parentId then 
+	 * that category created in Confluence via ConfluenceManager's CreatePage method.
+	 * After that getCategories method is called and then method calls itself and passes
+	 *  current category id and name as parentId and parentName.
+	 * 
+	 * @param project - object of project that is currently migrated.
+	 * @param parentName - name of parent of current category. First parentName equals "".
+	 * @param parentId - id of parent of current category. First parentId equals "".
+	 * @param categoriesArray - Array retrieved from categoriesMainObject. Contains list of all categories.
+	 */
 	public void getCategories(JSONObject project, String parentName, String parentId,
 			JSONArray categoriesArray) {
 		Iterator i = categoriesArray.iterator();
@@ -66,10 +90,17 @@ public class TeamworkDownloader {
 			}
 		}
 	}
-
+	
+	/**
+	 * At the beginnig method is getting array of files which belong to the passed project.
+	 * Then iterates over the array and searches for files which category-id is equals to passed category.
+	 * If finds one downloads it with ConfluenceManager's AddAttachmentToPage.
+	 * 
+	 * @param project - json object of file's project used 
+	 * @param category - json object of file's category
+	 */
 	public void getFilesFromCategory(JSONObject project, JSONObject category) {
-		JSONObject filesMainObject = (JSONObject) getAllFilesFromProject(url, apiToken,
-				project.getString("id"));
+		JSONObject filesMainObject = (JSONObject) getAllFilesFromProject(project.getString("id"));
 		JSONObject innerProject = (JSONObject) filesMainObject.get("project");
 		JSONArray filesArray = (JSONArray) innerProject.get("files");
 
@@ -78,7 +109,7 @@ public class TeamworkDownloader {
 			JSONObject singleFile = (JSONObject) k.next();
 
 			if (singleFile.get("category-id").equals(category.get("id"))) {
-				JSONObject finalFile = getFinalFileObject(url, apiToken, singleFile.getString("id"));
+				JSONObject finalFile = getFinalFileObject(singleFile.getString("id"));
 				JSONObject finalFileContent = (JSONObject) finalFile.get("file");
 				cm.AddAttatchmentToPage(project.getString("name"), category.getString("name"),
 						dfftw.DownloadFileFrom(finalFileContent.get("download-URL").toString(),
@@ -87,33 +118,55 @@ public class TeamworkDownloader {
 		}
 	}
 
-	private JSONObject getFinalFileObject(String urlS, String apiToken, String fileId) {
-		String credentials = apiToken + ":X";
-		return downloadJson(urlS, "files/" + fileId + ".json", credentials);
+	
+	/**
+	 * Returns JSONObject that contains all informations (name, id, category-id, project-id, download-URL) of file which id was passed.
+	 * @param fileId
+	 * @return
+	 */
+	private JSONObject getFinalFileObject(String fileId) {
+		return downloadJson("files/" + fileId + ".json");
 	}
 
-	public JSONObject getAllCategoriesFromProject(String urlS, String apiToken, String projectId) {
-		String credentials = apiToken + ":X";
-		return downloadJson(urlS, "projects/" + projectId + "/fileCategories.json", credentials);
+	/**
+	 * Returns JSONObject that contains all categories from project which id was passed.
+	 * @param projectId
+	 * @return
+	 */
+	public JSONObject getAllCategoriesFromProject(String projectId) {
+		return downloadJson("projects/" + projectId + "/fileCategories.json");
+	}
+	
+	/**
+	 * Returns JSONObject that contains all files from project which id was passed.
+	 * @param projectId
+	 * @return
+	 */
+	public JSONObject getAllFilesFromProject(String projectId) {
+		return downloadJson("projects/" + projectId + "/files.json");
 	}
 
-	public JSONObject getAllFilesFromProject(String urlS, String apiToken, String projectId) {
-		String credentials = apiToken + ":X";
-		return downloadJson(urlS, "projects/" + projectId + "/files.json", credentials);
+	/**
+	 * Returns JSONObject that contains all projects.
+	 * @return
+	 */
+	public JSONObject getAllProjects() {
+		return downloadJson("projects.json");
 	}
 
-	public JSONObject getAllProjects(String urlS, String apiToken) {
-		String credentials = apiToken + ":X";
-		return downloadJson(urlS, "projects.json", credentials);
-	}
-
-	public JSONObject downloadJson(String urlBeginning, String urlEnding, String credentials) {
+	/**
+	 * Connects to URL which contains JSON and returns it.
+	 * 
+	 * @param urlEnding - end of url from which you want to download JSON
+	 * @return JSONObject 
+	 */
+	public JSONObject downloadJson(String urlEnding) {
 		String jsonString = "";
 		JSONObject json;
 		try {
 			String encoding = "Basic "
 					+ new String(Base64.encodeBase64(credentials.getBytes("UTF-8")), "UTF-8");
-			URL url = new URL(urlBeginning + urlEnding);
+			URL url = new URL(this.urlBeginning + urlEnding);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Authorization", "Basic " + encoding);
