1f2b26763e66c90654047efad9d4c98e0bda5cb9
laikkk <k.zielinski@kainos.com>
2014-07-25 13:45:04 +0200

    modify my class, KZ

--- GetDataFromTeamWork/src/main/java/com/kainos/Migration/MyTeamWorkParser.java
+++ GetDataFromTeamWork/src/main/java/com/kainos/Migration/MyTeamWorkParser.java
@@ -3,6 +3,7 @@ package com.kainos.Migration;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -29,30 +30,78 @@ class MyManager {
 	public String teamworkUrl = "https://traineetestdel.teamwork.com/";
 
 	public MyManager() {
-		MYtreeBuldier mtbBuldier = new MYtreeBuldier();
-		System.out.println(mtbBuldier.getAllProjects(teamworkUrl, teamworkApiToken));
+		MYtreeBuldier mtbBuldier = new MYtreeBuldier(teamworkUrl,teamworkApiToken);
+		mtbBuldier.migrate();
 	}
 
 }
 
 class MYtreeBuldier {
 
-	public MYtreeBuldier() {
-
+	private String teamworkUrl ;
+	private String credentials ;
+	private ConfluenceManager confluenceManager;
+	
+	public MYtreeBuldier(String teamworkUrl,String teamworkApiToken) {
+		this.teamworkUrl = teamworkUrl;
+		credentials = teamworkApiToken + ":X";
+		confluenceManager = new ConfluenceManager();
+	}
+	
+	public void migrate(){
+		JSONArray projectsArray = getAllProjects().getJSONArray("projects");
+		System.out.println(projectsArray.toString());
+		
+		for (int i = 0; i < projectsArray.size(); i++)
+		{
+		    String projectName = projectsArray.getJSONObject(i).getString("name");
+		    String projectID = projectsArray.getJSONObject(i).getString("id");
+		    CreateStructureOfProject(projectName,projectID);
+		}
+		
+	}
+	
+	private void CreateStructureOfProject(String projectName,String projectID ) {
+		confluenceManager.CreateSpace(projectName);
+		//if project has categories (pages) create it,
+		JSONArray categoriesArray = getAllCategoriesFromProject(projectID).getJSONArray("categories");
+		CreateCategories(projectName,projectID,categoriesArray);
+		
 	}
 
-	public JSONObject getAllProjects(String urlS, String apiToken) {
-		String credentials = apiToken + ":X";
-		return downloadJson(urlS, "projects.json", credentials);
+	
+	private void CreateCategories(String projectName, String projectID, JSONArray categoriesArray) {
+		for (int i = 0; i < categoriesArray.size(); i++)
+		{
+	//	    String projectName = projectsArray.getJSONObject(i).getString("name");
+	//	    String projectID = projectsArray.getJSONObject(i).getString("id");
+		    CreateStructureOfProject(projectName,projectID);
+		}
+		
 	}
 
-	public JSONObject downloadJson(String urlBeginning, String urlEnding, String credentials) {
+	private JSONObject getFinalFileObject(String fileId) {
+			return downloadJson("files/" + fileId + ".json");
+	}
+
+	public JSONObject getAllCategoriesFromProject(String projectId) {
+		return downloadJson("projects/" + projectId + "/fileCategories.json");
+	}
+
+	public JSONObject getAllFilesFromProject(String projectId) {
+		return downloadJson("projects/" + projectId + "/files.json");
+	}
+
+	public JSONObject getAllProjects() {
+		return downloadJson("projects.json");
+	}
+
+	public JSONObject downloadJson(String urlEnding) {
 		String jsonString = "";
 		JSONObject json;
 		try {
-			String encoding = "Basic "
-					+ new String(Base64.encodeBase64(credentials.getBytes("UTF-8")), "UTF-8");
-			URL url = new URL(urlBeginning + urlEnding);
+			String encoding = "Basic "	+ new String(Base64.encodeBase64(credentials.getBytes("UTF-8")), "UTF-8");
+			URL url = new URL(teamworkUrl + urlEnding);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Authorization", "Basic " + encoding);
@@ -74,4 +123,5 @@ class MYtreeBuldier {
 		json = (JSONObject) JSONSerializer.toJSON(jsonString);
 		return json;
 	}
+	
 }
