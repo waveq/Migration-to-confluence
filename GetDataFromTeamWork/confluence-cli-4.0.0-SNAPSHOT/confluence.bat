4c53dd5d4145b7a2c013b75227407e64ffd506e4
waveq <nynonn@gmail.com>
2014-08-18 12:26:05 +0200

    cleaning properties

--- GetDataFromTeamWork/confluence-cli-4.0.0-SNAPSHOT/confluence.bat
+++ GetDataFromTeamWork/confluence-cli-4.0.0-SNAPSHOT/confluence.bat
@@ -7,20 +7,10 @@ set dirPath=%dirPath:\=/%
 rem remove blank at end of string
 set dirPath=%dirPath:~0,-1%
 
-set server=https://traineetest.atlassian.net/wiki/
-set username=nynon
-set password=tajnehaslo
+set server=
+set username=
+set password=
+
 
 rem - Customize for your installation, for instance you might want to add default parameters like the following:
 java -Xmx1g  -Xms512m -jar "%dirPath%"/lib/confluence-cli-4.0.0-SNAPSHOT.jar --server "%server%" --user "%username%" --password "%password%" %*
-
-rem java -Xmx8g -Xms2g -jar "%dirPath%"/lib/confluence-cli-4.0.0-SNAPSHOT.jar %*
-
-rem cd ..
-rem cd temp
-rem del *.* /Q
-rem cd ..
-rem exit
-
-rem Exit with the correct error level.
-rem EXIT /B %ERRORLEVEL%
